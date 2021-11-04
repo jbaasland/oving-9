47d0094015d10c853aafd5f03dd1d70f39e3d452
jbaasland <j.birk.aasland@gmail.com>
2021-11-04 19:35:52 +0100

    function fullført den e jawli bra asså fy faderullan begynt på if name = main greiå ikkje bra du e jo nesten ingenting der ka fillen

--- oppgave 9 quiz.py
+++ oppgave 9 quiz.py
@@ -6,38 +6,36 @@ Created on Fri Oct 29 13:42:11 2021
 """
 
 class quizlager:
-    def __init__(self, q, a, alt):
-        self.q = list()
-        self.a = list()
-        self.alt = list()
-    def __str__(self):
-        resultat = self.q
-        for i, alternativ in enumerate(self.a):
-            resultat += f"\n{i+1}: {alternativ}"
-        return resultat
-    def sjekk_svar(self, a):
-        if a == self.a:
-            return True
-        else:
-            return False
-    def korrekt_svar_tekst(self):
-        return self.alt[self.a]
-        
+	def __init__(self, q, a, alt):
+		self.q = q
+		self.a = a
+		self.alt = alt
+	def __str__(self):
+		resultat = self.q
+		for i, alternativ in enumerate(self.a):
+			resultat += f"\n{i+1}: {alternativ}"
+		return resultat
+	def sjekk_svar(self, a):
+		if a == self.a:
+			return True
+		else:
+			return False
+	def korrekt_svar_tekst(self):
+		answer = self.alt[self.a]
+		return f"Riktig svar er {answer}"
+		
+def qfunction():
+	qlist = []
+	with open("sporsmaalsfil.txt") as questions:
+		for i, qs in enumerate(questions):
+			splitqs = qs.split(":")
+			questions = splitqs[0].strip()
+			answer = int(splitqs[1].strip())
+			alternatives = splitqs[2].strip("][\n ")
+			alternatives = alternatives.split(", ")
+			qlist.append(quizlager(questions, answer, alternatives))
+	return qlist
 
-qdict = {}
-with open("sporsmaalsfil.txt") as questions:
-    for i in questions:
-        spliti = i.split(":")
-        questions = spliti[0].strip()
-        answer = int(spliti[1].strip())
-        alternatives = spliti[2].strip("][\n ")
-        print(alternatives)
-
-            
-            
-"""
-quiztest = quizlager(qs, ans, alts)
-for i in enumerate(qs):
-    print(quiztest)
-if __name__ = "__main__"
-"""
+if __name__ == "__main__":
+	qlista = qfunction()
+	print(qlista[0].korrekt_svar_tekst())
